9b762f9b3211d962667fbd2433f3603a7b1d6d48
thiagouglar16-dotcom <thiagouglar16@gmail.com>
2026-03-19 10:40:51 -0300

    Add includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</div>
+<footer class="sistema-footer">
+  <div><span style="color:gold;font-size:22px;">&#9670;</span> <strong>DR</strong>Info</div>
+  <div>© <?= date('Y') ?> Diamond Talents Brasil - DR Info</div>
+  <div>Todos os direitos reservados</div>
+  <div>Seu IP: <?= $_SERVER['REMOTE_ADDR'] ?? '' ?></div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="/js/scripts.js"></script>
+</body>
+</html>
